51d7442e7044949d36f38956a251f266bb8bf192
Keshav-Raj-098 <keshavraj09898@gmail.com>
2025-12-04 01:07:35 +0530

    new package installled

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.use(cors());
 
 app.use(bodyParser.json({ limit: "50mb" }));
 
+app.get("/",(req,res)=>{
+    res.send("PDF Service is running 🚀");
+})
 
 app.post("/download-resume/:resumeId/:userId", async (req, res, next) => {
   try {
